f4177150d9c8bd354c3ad738308bc7ebe627d067
LuckyCreator <44301423+LuckyCreator@users.noreply.github.com>
2020-06-27 19:25:46 +0200

    Ticket Update

--- commands/ticket.js
+++ commands/ticket.js
@@ -2,71 +2,74 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    const catagoryID = "726414717794386012";
-
-    var userName = message.author.username;
-    var userDiscriminator = message.author.discriminator;
-
-    var ticketBestaat = false;
-
-    message.guild.channels.cache.forEach(channel => {
-        
-        if(channel.name == userName.toLowerCase + "-" + userDiscriminator){
-            ticketBestaat = true;
-
-            message.reply("Je hebt al een bestaande ticket.");
-
-            return;
-        }
-
-    });
-
-    if(ticketBestaat) return;
-
-    var embed = new discord.MessageEmbed()
-        .setTitle("Hoi " + message.author.userName)
-        .setColor("#fc7703")
-        .setFooter("Je ticket wordt aangemaakt.", "https://i.imgur.com/ypA0sc3.png");
     
-    message.channel.send(embed);
+    const reason = message.content.split(" ").slice(1).join(" ");
 
-    message.guild.channels.create(message.guild.userName.toLowerCase + "-" + userDiscriminator, {type: "text"}).then(
-        (createdChannel) => {
-            createdChannel.setParent(catagoryID).then(
-                (settedParent) => {
+    let SupportCategory = message.guild.channels.cache.find(category => category.name === "Tickets");
 
-                    settedParent.updateOverwrite(message.guild.roles.cache.find(x => x.name === '@everyone'),{
-                        SEND_MESSAGES: false,
-                        VIEW_CHANNEL: false
-                    });
+    if(message.guild.me.hasPermission(`MANAGE_CHANNELS`) && !SupportCategory) {
+        SupportCategory = await message.guild.channels.createChannel(`Tickets`, {
+            type: "category",
+        });
+    };
 
-                    settedParent.updateOverwrite(message.author.id,{
-                        SEND_MESSAGES: true,
-                        VIEW_CHANNEL: true,
-                        READ_MESSAGES: true,
-                        CREATE_INSTANT_INVITE: false,
-                        ATTACH_FILES: true,
-                        CONNECT: true,
-                        ADD_REACTIONS: true,
-                        READ_MESSAGES_HISTORY: true
-                    });
+    if(!message.guild.me.hasPermission(`MANAGE_CHANNELS`) && !SupportCategory) {
+        message.guild.send("Sorry maar je hebt hier helaas geen rechten voor.")
+    }
 
-                    var embedParent = new discord.MessageEmbed()
-                        .setTitle(`Hoi ${message.author.username}`)
-                        .setColor("#fc7703")
-                        .setDescription("Stel hier je vraag en je zult zo spoedig mogelijk geholpen worden.")
-                        .setFooter("play.quadgames.ga | QuadGames", "https://i.imgur.com/ypA0sc3.png")
+    if(!message.guild.roles.cache.find(role => role.name === "Support Team")) {
+        await (message.guild.roles.create({
+            name: `Support Team`,
+            color: `YELLOW`,
+        }));
+    };
 
-                    settedParent.send(embedParent);
+    let supportrole = message.guild.roles.cache.find(role => role.name === "Support Team")
 
-                }
-            ).catch(err => {
-                message.channel.send("Er is iets fout gegaan!");
-            });
-        }
-    ).catch(err => {
-        message.channel.send("Er is iets fout gegaan!");
-    });
+    if(!supportrole) {
+        return message.channel.send("Sorry er is wat mis gegaan. Vraag AlwaysLucky_#2666 voor hulp!")
+    }
+
+    if(!reason) {
+        return message.channel.send("Geef een reden op om een ticket aan te kunnen maken.")
+    }
+
+    const channelName = `ticket-${message.author.username}-${message.auhtor.discriminator}`
+    if(message.guild.channels.cache.find(channel => channel.name === `ticket-${message.author.username.toLowerCase()}-${message.author.discriminator}`)) {
+        return message.channel.send("Sorry maar je hebt al een lopende ticket.")
+    }
+
+    message.guild.channels.create(channelName, { parent: SupportCategory.id, topic: `Ticket Owner: ${message.author.id}`}).then(c => {
+        const sr = message.guild.roles.cache.get(supportrole)
+        const everyone = message.guild.roles.cache.find(role => role.name === `@everyone`)
+        c.updateOverwrite(sr, {
+            SEND_MESSAGES: true,
+            VIEW_CHANNEL: true,
+        });
+        c.updateOverwrite(everyone, {
+            SEND_MESSAGES: false,
+            VIEW_CHANNEL: false,
+        });
+        c.updateOverwrite(message.author, {
+            SEND_MESSAGES: true,
+            VIEW_CHANNEL: true,
+        });
+        let CreatedTicketEmbed = new discord.MessageEmbed()
+            .setColor("#fc7703")
+            .setTitle("Nieuw Support Ticket")
+            .setDescription(`<@${message.author.id}> Je support ticket kanaal is <#${c.id}`)
+            .setTimestamp()
+            .setFooter("play.quadgames.ga | QuadGames", "https://i.imgur.com/ypA0sc3.png")
+        message.channel.send(CreatedTicketEmbed)
+        let GreetEmbed = new discord.MessageEmbed()
+            .setColor("#fc7703")
+            .addField(`Nieuw Support Ticket`, `<@${message.auhtor.id}> Bedankt voor het aanmaken van een ticket. Je wordt zo spoedig mogelijk geholpen.`)
+            .addField(`Probleem:`, reason)
+            .setTimestamp()
+            .setFooter("play.quadgames.ga | QuadGames", "https://i.imgur.com/ypA0sc3.png")
+        c.send(GreetEmbed)
+
+    }).catch(console.error);
 
 
 }
--- commands/ticketClose.js
+++ commands/ticketClose.js
@@ -2,26 +2,12 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    const catagoryID = "726414717794386012";
-
-    if(!message.member.hasPermission("KICK_MEMBER")) return message.reply("Jij kan dit helaas niet doen.");
-
-    if(message.channel.parentID == catagoryID) {
+    if(message.guild.me.hasPermission(`MANAGE_CHANNELS`) && !SupportCategory) {
         message.channel.delete();
+    }
 
-        var embedCreateTicket = new discord.MessageEmbed()
-            .setTitle("Ticket, " + message.channel.name)
-            .setColor("#fc7703")
-            .setDescription("De ticket is gemarkeerd als **Voltooid**")
-            .setFooter("play.quadgames.ga | QuadGames", "https://i.imgur.com/ypA0sc3.png")
-    
-        var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "log")
-        if (!ticketChannel) return message.reply("Kanaal bestaat niet.");
-
-        ticketChannel.send(embedCreateTicket);
-
-    } else {
-        message.channel.send("Dit command kan enkel in een ticket kanaal worden uitgevoerd.")
+    if(!message.guild.me.hasPermission(`MANAGE_CHANNELS`) && !SupportCategory) {
+        message.guild.send("Sorry maar je hebt hier helaas geen rechten voor.")
     }
 
 }
