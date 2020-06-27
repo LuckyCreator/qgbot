d76ddf67a0741b37383f56b87e4bf4ca38881c4f
LuckyCreator <44301423+LuckyCreator@users.noreply.github.com>
2020-06-27 15:27:33 +0200

    test2
    test

--- commands/ticket.js
+++ commands/ticket.js
@@ -9,7 +9,7 @@ module.exports.run = async(bot, message, args) => {
 
     var ticketBestaat = false;
 
-    message.guild.channels.cache.forEach(channel => {
+    message.guild.channels.forEach(channel => {
         
         if(channel.name == userName.toLowerCase() + "-" + userDiscriminator){
             ticketBestaat = true;
