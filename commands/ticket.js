717fcd15336e343684bd8b1a177b4ffb7faf1eb3
LuckyCreator <44301423+LuckyCreator@users.noreply.github.com>
2020-06-27 19:36:35 +0200

    test

--- commands/ticket.js
+++ commands/ticket.js
@@ -34,7 +34,7 @@ module.exports.run = async(bot, message, args) => {
         return message.channel.send("Geef een reden op om een ticket aan te kunnen maken.")
     }
 
-    const channelName = `ticket-${message.author.userName}-${message.auhtor.discriminator}`
+    const channelName = `ticket-${member.author.userName}-${message.auhtor.discriminator}`
     if(message.guild.channels.cache.find(channel => channel.name === `ticket-${message.author.username.toLowerCase()}-${message.author.discriminator}`)) {
         return message.channel.send("Sorry maar je hebt al een lopende ticket.")
     }
